4e39607b26037f4310488122de82ed48e10ca0c7
Jenny Giraldo <jenny.giraldo.vasquez@gmail.com>
2022-04-20 14:07:12 -0500

    proyecto platzi conf

---
+++ index.js
@@ -0,0 +1,6 @@
+$ (function (){
+$('[data-toggle="tooltip"]') 
+})
+
+
+// pluggin de jquery que agrega bootstrap para referenciar mensajes flotantes cuando uno paalabra seleccionada no se conoce. en este caso ignite
